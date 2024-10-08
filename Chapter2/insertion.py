3995bc99b2098b4584530c4b450198d6ff847a3c
Nika <70407540+NikaAlgorithm@users.noreply.github.com>
2024-10-08 18:14:50 +0330

    Update insertion.py

--- Chapter2/insertion.py
+++ Chapter2/insertion.py
@@ -3,12 +3,9 @@ def insertionSort(arr : list) -> list:
     n = len(arr)
     
     for j in range(1,n):
-        
         key = arr[j]
         i = j-1
-        
         while (i >=0) and (key <= arr[i]):
-
             arr[i+1] = arr[i]
             i = i - 1
             
